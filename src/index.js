252a2de7f74ecfde42476e158f4a8a1b473a475f
Kim <kim.schmider@hotmail.com>
2018-07-12 12:02:32 +0200

    Rename source file and use factory function for exporting

---
+++ src/index.js
@@ -0,0 +1,43 @@
+(function(root, factory) {
+  if (typeof exports === 'object')
+    module.exports = factory();
+  else
+    root.VueDetails = factory();
+})(this, function() {
+  return {
+    name: 'v-details',
+    render(h) {
+      return h('details', {
+        on: {
+          toggle: this.toggle
+        }
+      }, this.$slots.default);
+    },
+    model: {
+      prop: 'open',
+      event: 'change'
+    },
+    watch: {
+      open(val) {
+        this.updateVal(val);
+      }
+    },
+    props: {
+      open: Boolean
+    },
+    methods: {
+      toggle(e) {
+        this.$emit('change', e.target.open);
+      },
+      updateVal(open) {
+        if (open)
+          this.$el.setAttribute('open', '');
+        else
+          this.$el.removeAttribute('open');
+      }
+    },
+    mounted() {
+      this.updateVal(this.open);
+    }
+  } 
+});
